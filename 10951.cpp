ef9d2e80283de4e5be1321d05bb2f7cf73b9c3d9
wkddbwls801 <wkddbwls801@naver.com>
2021-12-19 23:30:34 +0900

    10951.cpp

---
+++ 10951.cpp
@@ -0,0 +1,18 @@
+// A+B - 4
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int A;
+	int B;
+
+	while (cin >> A >> B) {
+		cout << A + B << '\n';
+	}
+
+	return 0;
+}
