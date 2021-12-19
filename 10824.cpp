317377a4861bded9c4d8e179c7df2e710b580f7b
wkddbwls801 <wkddbwls801@naver.com>
2021-12-19 23:29:59 +0900

    10824.cpp

---
+++ 10824.cpp
@@ -0,0 +1,24 @@
+// ³× ¼ö
+
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio;
+	cin.tie(NULL);
+
+	string arr[4];
+	unsigned long long output1;
+	unsigned long long output2;
+
+	for (int i = 0; i < 4; i++) {
+		cin >> arr[i];
+	}
+
+	output1 = stoi(arr[0]) * pow(10, arr[1].size()) + stoi(arr[1]);
+	output2 = stoi(arr[2]) * pow(10, arr[3].size()) + stoi(arr[3]);
+
+	cout << output1 + output2 << "\n";
+}
