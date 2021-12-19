17a682142e7c42b79e597907da9db92f5ee9c131
wkddbwls801 <wkddbwls801@naver.com>
2021-12-19 23:33:18 +0900

    15552.cpp

---
+++ 15552.cpp
@@ -0,0 +1,22 @@
+// 빠른 A+B
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T;	//테스트 케이스
+	int A;
+	int B;
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+
+	return 0;
+}
