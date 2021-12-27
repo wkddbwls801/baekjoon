db00edf1907c54be8e12eb273371416a7b712612
wkddbwls801 <wkddbwls801@naver.com>
2021-12-27 21:34:28 +0900

    1436.cpp

---
+++ 1436.cpp
@@ -0,0 +1,41 @@
+// 영화감독 숌
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int N;
+
+int FindSeries(int N) {
+	int i = 666;	// 제일 작은 수
+	int series = 0;	// count
+	string target;
+
+	while (1) {
+		target = to_string(i);
+
+		for (int j = 0; j < target.length() - 2; j++) {
+			if (target[j] == '6' && target[j + 1] == '6' && target[j + 2] == '6') {
+				series++;
+
+				if (series == N) {
+					return i;
+				}
+
+				break;
+			}
+		}
+
+		i++;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	cout << FindSeries(N);
+
+}
