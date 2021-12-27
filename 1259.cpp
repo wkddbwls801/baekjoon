ee59d00a4418fe8aeff83f12a4c7f607c71a495b
wkddbwls801 <wkddbwls801@naver.com>
2021-12-27 21:34:22 +0900

    1259.cpp

---
+++ 1259.cpp
@@ -0,0 +1,37 @@
+// ÆÓ¸°µå·Ò¼ö
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+string num;
+bool check;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+
+		cin >> num;
+
+		if (stoi(num) == 0) {
+			return 0;
+		}
+
+		check = false;
+		for (int i = 0; i < num.size() / 2; i++) {
+			if (num[i] != num[num.size() - 1 - i]) {
+				cout << "no" << "\n";
+				check = true;
+				break;
+			}
+		}
+
+		if (check == false) {
+			cout << "yes" << "\n";
+		}
+	}
+}
