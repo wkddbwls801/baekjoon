f489007ae7ea081727fc1ec0774cf859e1d94b84
wkddbwls801 <wkddbwls801@naver.com>
2022-01-12 21:11:12 +0900

    11727.cpp

---
+++ 11727.cpp
@@ -0,0 +1,24 @@
+// 2*n ≈∏¿œ∏µ 2
+
+#include<iostream>
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 3;
+	
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+	}
+
+	cout << dp[n] % 10007 << "\n";
+}
