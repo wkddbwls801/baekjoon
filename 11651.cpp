720d77b9ae1c228a68ab5f4d80565f320e5f10c3
wkddbwls801 <wkddbwls801@naver.com>
2022-01-12 21:10:42 +0900

    11651.cpp

---
+++ 11651.cpp
@@ -0,0 +1,44 @@
+// 좌표 정렬하기 2
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+struct point {
+	int x;
+	int y;
+};
+
+bool operator<(point a, point b) {
+	if (a.y == b.y) {
+		return a.x < b.x;
+	}
+
+	return a.y < b.y;
+}
+
+int N;
+vector<point> p;
+int p_x;
+int p_y;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> p_x >> p_y;
+
+		p.push_back({ p_x, p_y });
+	}
+
+	sort(p.begin(), p.end());
+
+	for (int i = 0; i < N; i++) {
+		cout << p[i].x << " " << p[i].y << "\n";
+	}
+}
